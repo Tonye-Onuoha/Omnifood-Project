bb7230ffa6dac3b0f4a05c548c1aa1635c252a11
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2026-01-09 23:04:14 +0100

    Completed mobile navigation functionality

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Set current year
+const year = document.querySelector(".current-year");
+const date = new Date();
+const currentYear = date.getFullYear();
+year.textContent = currentYear;
+
+// Make navigation work
+const mobileNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+mobileNav.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
+
+// Smooth scrolling
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute("href");
+
+    if (href === "#") {
+      // Scroll back to top
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+    if (href.startsWith("#") && href.length > 1) {
+      // Scroll to other links
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+    }
+    // Close mobile navigation
+    if (e.currentTarget.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
